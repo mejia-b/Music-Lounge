09bc8f138476bda0eab5807d3d3d71e82e070f6c
Brenda Mejia <mejiab19@gmail.com>
2022-11-27 01:59:32 -0600

    Implement function for button logic, POST method

--- music_project/frontend/src/components/CreateRoomPage.js
+++ music_project/frontend/src/components/CreateRoomPage.js
@@ -29,7 +29,18 @@ export default class CreateRoomPage extends Component{
     }
 
     handleRoomButtonPressed(){
-        console.log(this.state);
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_pause: this.state.guestCanPause,
+            }),
+        };
+
+        fetch('/api/create-room', requestOptions).then((response) =>
+            response.json()
+        ).then((data) => console.log(data));
 
     }
 
