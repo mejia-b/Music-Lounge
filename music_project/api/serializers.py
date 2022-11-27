ba0fccf7e97eb34bc3f3b0866adea706d147f033
Brenda Mejia <mejiab19@gmail.com>
2022-11-26 23:29:06 -0600

    Create another view in api app to handle POST Requests

--- music_project/api/serializers.py
+++ music_project/api/serializers.py
@@ -6,3 +6,8 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_pause',
                   'votes_to_skip', 'created_at')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Room
+        fields = ('guest_pause', 'votes_to_skip')
